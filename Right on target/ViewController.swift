3c3486febfe7500c11003dc7953489efa644b488
Dmitriy <skokdmitriy@gmail.com>
2022-04-24 00:39:07 +0700

    ver.1.1

--- Right on target/ViewController.swift
+++ Right on target/ViewController.swift
@@ -7,13 +7,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController{
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var slider: UISlider!
+    
+    var number: Int = 0
+    var round: Int = 1
+    var points: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print ("viewDidLoad")
+        number = Int.random(in: 1...50)
+        label.text = String(number)
     }
-
-
+    
+    @IBAction func checkNumber(_ sender: Any) {
+        let numSlider = Int(slider.value)
+        print(numSlider)
+        if numSlider > number {
+            points += 50 - numSlider + number
+        } else if numSlider < number {
+            points += 50 - numSlider + number
+        } else {
+            points += 50
+        }
+        print("Очки \(points)")
+        
+        if round == 5{
+            let alert = UIAlertController(title: "Игра окончена!",
+                                          message: "Вы заработали \(points) очков",
+                                          preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Начать заново",
+                                          style: .default,
+                                          handler: nil))
+            present(alert, animated: true, completion: nil)
+            round = 1
+            points = 0
+        } else {
+            round += 1
+        }
+        number = Int.random(in: 1...50)
+        label.text = String(number)
+    }
 }
-
